fb135f41108a3bf180834baf89cf13b99e901abe
Christopher Haddox <cmhaddox@asu.edu>
2020-11-18 23:16:03 -0700

    Fixed small coloring issue

--- script.js
+++ script.js
@@ -4,43 +4,43 @@ function riskRating() {
     if(rand > -1 && rand < 10) {
         document.getElementById('rating').innerHTML = rand;
         document.getElementById('rating').style.color = "#00cc99";
-        document.getElementById('rating').style.background = black;
+        document.getElementById('rating').style.backgroundColor = black;
     } else if(rand > 10 && rand < 20) {
         document.getElementById('rating').innerHTML = rand;
         document.getElementById('rating').style.color = "#00ff99";
-        document.getElementById('rating').style.background = black;
+        document.getElementById('rating').style.backgroundColor = black;
     } else if(rand > 20 && rand < 30) {
         document.getElementById('rating').innerHTML = rand;
         document.getElementById('rating').style.color = "#66ff99";
-        document.getElementById('rating').style.background = black;
+        document.getElementById('rating').style.backgroundColor = black;
     } else if(rand > 30 && rand < 40) {
         document.getElementById('rating').innerHTML = rand;
         document.getElementById('rating').style.color = "#99ff99";
-        document.getElementById('rating').style.background = black;
+        document.getElementById('rating').style.backgroundColor = black;
     } else if(rand > 40 && rand < 50) {
         document.getElementById('rating').innerHTML = rand;
         document.getElementById('rating').style.color = "#ccff99";
-        document.getElementById('rating').style.background = black;
+        document.getElementById('rating').style.backgroundColor = black;
     } else if(rand > 50 && rand < 60) {
         document.getElementById('rating').innerHTML = rand;
         document.getElementById('rating').style.color = "#ffff99";
-        document.getElementById('rating').style.background = black;
+        document.getElementById('rating').style.backgroundColor = black;
     } else if(rand > 60 && rand < 70) {
         document.getElementById('rating').innerHTML = rand;
         document.getElementById('rating').style.color = "#ffcc66";
-        document.getElementById('rating').style.background = black;
+        document.getElementById('rating').style.backgroundColor = black;
     } else if(rand > 70 && rand < 80) {
         document.getElementById('rating').innerHTML = rand;
         document.getElementById('rating').style.color = "#ff9933";
-        document.getElementById('rating').style.background = black;
+        document.getElementById('rating').style.backgroundColor = black;
     } else if(rand > 80 && rand < 90) {
         document.getElementById('rating').innerHTML = rand;
         document.getElementById('rating').style.color = "#ff3300";
-        document.getElementById('rating').style.background = black;
+        document.getElementById('rating').style.backgroundColor = black;
     } else if(rand > 90 && rand < 101) {
         document.getElementById('rating').innerHTML = rand;
         document.getElementById('rating').style.color = "#990000";
-        document.getElementById('rating').style.background = black;
+        document.getElementById('rating').style.backgroundColor = black;
     }
 
 }
